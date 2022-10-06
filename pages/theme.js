12237c4b655c21306b2a5a187ff7ba54e9c942e4
Dusk Light <r.rahul.developer@gmail.com>
2022-10-06 12:48:01 +0530

    configured fonts

---
+++ pages/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Wotfard', sans-serif`,
+    body: `'Wotfard', sans-serif`,
+  },
+});
+
+export default theme;
